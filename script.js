f5d7b07de3369d15723667130be2cabed2a3deb3
MG-Mahi <68122614+MG-Mahi@users.noreply.github.com>
2021-01-23 08:51:36 +0530

    New landing page website

---
+++ script.js
@@ -0,0 +1,7 @@
+let toggleButton = document.getElementsByClassName("toggle-button")[0];
+
+let navLinks = document.getElementsByClassName("nav-links")[0];
+
+toggleButton.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+})
